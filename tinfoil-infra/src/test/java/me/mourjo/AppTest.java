69652858a96d631bc77d8ecedf7c4485ad7760d2
Mourjo Sen <hello@mourjo.me>
2024-07-31 07:03:17 +0200

    Initial commit

---
+++ tinfoil-infra/src/test/java/me/mourjo/AppTest.java
@@ -0,0 +1,12 @@
+package me.mourjo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void testme() {
+        Assertions.assertEquals(1,4-3);
+    }
+}
---
+++ tinfoil-web/src/main/java/me/mourjo/App.java
@@ -0,0 +1,11 @@
+package me.mourjo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World from Web!" );
+    }
+}
