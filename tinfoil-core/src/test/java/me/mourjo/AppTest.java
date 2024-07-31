723a80190d7bb09b40aa0bfea9482e3002ab8db0
Mourjo Sen <hello@mourjo.me>
2024-07-31 11:37:45 +0200

    Revert "Remove all tests"
    This reverts commit 521680e662e1d058a7864cf79bdd12d8c6026bfc.

---
+++ tinfoil-core/src/test/java/me/mourjo/AppTest.java
@@ -0,0 +1,14 @@
+package me.mourjo;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+	@Test
+	void testme() {
+		Assertions.assertEquals(2, 4 - 2);
+	}
+
+}
