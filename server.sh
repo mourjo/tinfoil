6bc630739bace86aaebe8f75fb40f46231d540ac
Mourjo Sen <hello@mourjo.me>
2024-08-01 17:54:37 +0200

    Reduce waittime in server start

--- server.sh
+++ server.sh
@@ -7,6 +7,6 @@ trap "kill 0" EXIT
 java -cp tinfoil-web/target/tinfoil-web-1.0-SNAPSHOT.jar me.mourjo.main.Server > /dev/null &
 
 PID=$!
-sleep 10;
+sleep 5;
 
 curl -s -X 'POST' 'http://localhost:7002/visit/albert-heijn/mourjo' && curl -s -X 'GET' 'http://localhost:7002/visits/all/mourjo'
