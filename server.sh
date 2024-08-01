e95d0e3c19dc58d4f12693b950fa2eaf710d5ba8
Mourjo Sen <hello@mourjo.me>
2024-08-01 17:51:57 +0200

    Github action to run server

---
+++ server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
+#mvn clean package
+java -cp tinfoil-web/target/tinfoil-web-1.0-SNAPSHOT.jar me.mourjo.main.Server > /dev/null &
+
+PID=$!
+sleep 10;
+
+curl -s -X 'POST' 'http://localhost:7002/visit/albert-heijn/mourjo' && curl -s -X 'GET' 'http://localhost:7002/visits/all/mourjo'
